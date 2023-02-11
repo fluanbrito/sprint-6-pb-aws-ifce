34d4833bc47a0da9e267bc9f2ed7c8720c9cd25e
Dayanne Bugarim <dayannebugarim1@gmail.com>
2023-02-11 19:06:51 -0300

    feat: conversão e armazenamento no s3

--- api-tts/handler.py
+++ api-tts/handler.py
@@ -1,41 +1,73 @@
-import json
 import boto3
-from flask import Flask, request, jsonify
-from flask_cors import CORS
+import urllib.parse
+import json
+import datetime
 
-app = Flask(__name__)
-CORS(app)
 
-polly = boto3.client('polly')
-s3 = boto3.client('s3')
+def v1_form(event, context):
+    with open("form.html", "r") as file:
+        content = file.read()
+        return {
+            'statusCode': 200,
+            'body': content,
+            'headers': {
+                'Content-Type': 'text/html'
+            }
+        }
 
-@app.route('/v1', methods=['GET'])
-def v1_form():
-    with open('form.html') as file:
-        return file.read()
+def v1_tts(event, context):
+    try: 
+        # recebe a frase do formulário
+        body = event['body']
+        parsed_body = urllib.parse.parse_qs(body)
+        phrase = parsed_body.get('phrase')[0]
+        print(phrase)
 
-@app.route('/v1/tts', methods=['POST'])
-def v1_tts():
-    received_phrase = request.form['phrase']
-    response = polly.synthesize_speech(
-        Text=received_phrase,
-        OutputFormat='mp3',
-        VoiceId='Joanna'
-    )
-    audio = response['AudioStream'].read()
+        # gera o audio usando o polly
+        polly = boto3.client('polly')
+        response = polly.synthesize_speech(
+            Text=phrase,
+            VoiceId='Vitoria',
+            OutputFormat='mp3'
+        )
 
-    s3.put_object(
-        Bucket='api-tts-audio-storage',
-        Body=audio,
-        Key=f'{received_phrase}.mp3'
-    )
+        # formatação para o nome do arquivo e url
+        file_name = 'audio_' + phrase[:20].replace(" ", "_") + '.mp3'
 
-    url = f'https://s3.amazonaws.com/api-tts-audio-storage/{received_phrase}.mp3'
-    return jsonify({
-        'received_phrase': received_phrase,
-        'url_to_audio': url,
-        'created_audio': 'data não disponível'
-    })
+        # salva o audio no bucket
+        s3 = boto3.client('s3')
+        s3.put_object(
+            Bucket='api-tts-audio-storage',
+            Key=file_name,
+            Body=response['AudioStream'].read()
+        )
 
-def handler(event, context):
-    return app(event, context)
+        # formatação da data
+        date_string = response['ResponseMetadata']['HTTPHeaders']['date']
+        date_object = datetime.datetime.strptime(date_string, '%a, %d %b %Y %H:%M:%S %Z')
+        formatted_date = date_object.strftime('%d-%m-%Y %H:%M:%S')
+
+        # retorna a resposta em json formatado
+        return {
+            'statusCode': 200,
+            'headers': {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            },
+            'body': json.dumps({
+                'received_phrase': phrase,
+                'url_to_audio': f'https://api-tts-audio-storage.s3.amazonaws.com/{file_name}',
+                'created_audio': formatted_date
+            })
+        }
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'headers': {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            },
+            'body': json.dumps({
+                'error': str(e)
+            })
+        }
