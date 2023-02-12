720333f2646eb2c3350348cb1a7203c9e73c9a86
Josiana Silva <josianasilva655@gmail.com>
2023-02-12 14:05:30 -0300

    Correções no código

--- api-tts/v1.py
+++ api-tts/v1.py
@@ -5,35 +5,28 @@ import os
 from contextlib import closing
 from datetime import datetime
 
-with open("api-tts/.config/credentials.json", "r") as file:
-    credentials = json.load(file) 
-print("teste")
-print(credentials.aws_secret_access_key)
 
 def v1_tts(event, context):
+
     postId = str(uuid.uuid4())
-
     text = event["phrase"]
+
     with open("../.config/credentials.json", "r") as file:
-        credentials = json.load(file) 
-    print("teste")
+        credentials = json.load(file)
 
-    print(credentials.aws_secret_access_key)
     polly = boto3.Session(
-                aws_access_key_id=credentials["aws_access_key_id"],                     
-                aws_secret_access_key=credentials["aws_secret_access_key"],
-                region_name='us-east-1').client('polly')
+        aws_access_key_id=credentials["aws_access_key_id"],
+        aws_secret_access_key=credentials["aws_secret_access_key"],
+        region_name='us-east-1').client('polly')
 
-    
     response = polly.synthesize_speech(
         OutputFormat='mp3',
         Text=text,
         VoiceId="Camila"
     )
-    
-    
+
     if "AudioStream" in response:
-        with closing (response["AudioStream"]) as stream:
+        with closing(response["AudioStream"]) as stream:
             output = os.path.join("/tmp/", postId)
             with open(output, "ab") as file:
                 file.write(stream.read())
@@ -45,18 +38,16 @@ def v1_tts(event, context):
     s3.put_object_acl(ACL='public-read',
                       Bucket=os.environ['BUCKET_NAME'],
                       Key=postId + ".mp3")
-    
-    
-    url_begining = "https://s3-" + 'us-east-1' + ".amazonaws.com/"
+
+    url_begining = "https://s3-us-east-1.amazonaws.com/"
     url = url_begining \
         + str(os.environ['BUCKET_NAME']) \
         + "/tmp/" \
         + str(postId) \
-        + ".mp3" 
-        
-        
-    return  {
-    "received_phrase": text,
-    "url_to_audio": url,
-    "created_audio": f"{datetime.now()}"
-  }
+        + ".mp3"
+
+    return {
+        "received_phrase": text,
+        "url_to_audio": url,
+        "created_audio": f"{datetime.now()}"
+    }
