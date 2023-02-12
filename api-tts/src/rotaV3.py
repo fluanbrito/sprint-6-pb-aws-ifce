f442cd4caab5eeaa00b052a30195c3debc7931f0
Samara Alcantara <samara31oliveira@gmail.com>
2023-02-12 18:35:14 -0300

    rotas v2 e v3

---
+++ api-tts/src/rotaV3.py
@@ -0,0 +1,65 @@
+import boto3
+import os
+import uuid
+import json
+import logging
+
+
+#criação da tabela
+def get_dynamo_table():
+    dynamodb = boto3.resource("dynamodb")
+    table_name = os.environ['TabelaPolly']
+    return dynamodb.Table(table_name)
+
+#Pesquisando Itens
+def list(event, context):
+    table = get_dynamo_table()
+    return table.scan()['Items']
+
+#Modo create
+def list(event, context):
+    body = event["body"]
+    table = get_dynamo_table()
+    lista = table.scan()['Items']
+    for e in lista:
+
+        if("id" in body):
+            body = {
+                "received_phrase": "converta esse texto para áudio",
+                "url_to_audio": "https://meu-buckect/audio-xyz.mp3",
+                "created_audio": "02-02-2023 17:00:00",
+                "unique_id": "123456"
+                }
+            response = {"status": 200,
+                "body": json.dumps(body)}
+            return response
+
+                
+        elif ("frase" in body and "url" in body):
+            table = get_dynamo_table()
+
+            table.put_item(
+                Item={
+                    "id":    str(uuid.uuid4()),
+                    "frase":  body["frase"],
+                    "url": body["url"],
+                }
+            )
+        
+            body = {
+                
+            "received_phrase": "converta esse texto para áudio",
+            "url_to_audio": "https://meu-buckect/audio-xyz.mp3",
+            "created_audio": "02-02-2023 17:00:00",
+            "unique_id": "123456"
+        }
+            
+        response = {"status": 200,
+                "body": json.dumps(body)}
+
+        return response
+    else:
+        return {"status": 500,
+                "body": "Error"}
+    
+    
