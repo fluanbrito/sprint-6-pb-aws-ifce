3c12fba773a0c9355d20426afaab052c169a2d00
mylensoares <mylenateles@hotmail.com>
2023-02-13 16:39:52 -0300

    corrigindo erro no s3

--- api-tts/src/rotaV1.py
+++ api-tts/src/rotaV1.py
@@ -7,34 +7,37 @@ polly_client = boto3.client('polly')
 s3_client = boto3.client('s3')
 
 def tts(event, context):
+    try:
 
-    phrase = json.loads(event['body'])['phrase']
+        phrase = json.loads(event['body'])['phrase']
 
-    response = polly_client.synthesize_speech(
-        OutputFormat='mp3',
-        Text=phrase,
-        VoiceId='Vitoria'
-    )
+        response = polly_client.synthesize_speech(
+            OutputFormat='mp3',
+            Text=phrase,
+            VoiceId='Vitoria'
+        )
 
-    audio = response['AudioStream'].read()
+        audio = response['AudioStream'].read()
+        phrase = unicodedata.normalize('NFKD', str(phrase).replace(" ", ""))
+        phrase = "".join([c for c in phrase if not unicodedata.combining(c)])
+        
+        s3_client.put_object(
+            Body=audio,
+            Bucket='bucketpollysprint6',
+            Key=f'{phrase}.mp3',
+        )
 
-    s3_client.put_object(
-        Body=audio,
-        Bucket='bucketpollysprint6',
-        Key=f'{phrase}.mp3',
-    )
-
-    time = datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S")
-
-    phrase = unicodedata.normalize('NFKD', str(phrase).replace(" ", ""))
-    phrase = "".join([c for c in phrase if not unicodedata.combining(c)])
-    
-    return {
-        'statusCode': 200,
-        'body': json.dumps({'message': f'Frase convertida para audio com sucesso!',
-                           'audio_url': f'https://s3.amazonaws.com/bucketpollysprint6/{phrase}.mp3',
-                           'timestamp': time})
-    }
+        time = datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S")
+        
+        return {
+            'statusCode': 200,
+            'body': json.dumps({'message': f'Frase convertida para audio com sucesso!',
+                            'audio_url': f'https://s3.amazonaws.com/bucketpollysprint6/{phrase}.mp3',
+                            'timestamp': time})
+        }
+    except:
+        return {"status": 500,
+                    "body": "Error"}
 
 
 
