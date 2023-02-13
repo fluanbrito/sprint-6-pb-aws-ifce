b41ce5500ff7982f6d0157b20b04da5b0b3452e6
Jhonatan Gonçalves Pereira <jhonatan.goncalves08@aluno.ifce.edu.br>
2023-02-13 10:02:06 -0300

    :sparkles: feat: página HTML e modificação do app.py

--- api-tts/src/app.py
+++ api-tts/src/app.py
@@ -1,17 +1,25 @@
-from flask import Flask, render_template
+# Importando bibliotecas
+from flask import Flask, request, render_template
+import boto3
 
+#
 app = Flask(__name__)
 
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-
-@app.route('/api/data')
-def api_data():
-    # aqui você pode retornar os dados da sua API em formato JSON
-    return {'data': 'dados da sua API'}
+# Criando rota e conectanto ao serviço aws polly
+@app.route('/v1/tts', methods=['POST'])
+def process():
+    text = request.form['text']
+    client = boto3.client('polly')
+    response = client.synthesize_speech(
+        OutputFormat='mp3',
+        Text=text,
+        VoiceId='Joana'
+    )
+    # exibindo o audio para o user
+    file = open('speech.mp3', 'wb')
+    file.write(response['AudioStream'].read())
+    file.close()
+    return render_template('speech.html')
 
 
 if __name__ == '__main__':
